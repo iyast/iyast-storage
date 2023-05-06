38118eac594e91dcd62a90ef1d499f8708335ac9
iyast <121828546+iyast@users.noreply.github.com>
2023-05-07 00:39:05 +0300

    Update aIAT-iyas.js

--- aIAT-iyas.js
+++ aIAT-iyas.js
@@ -3,9 +3,9 @@ define(['pipAPI', 'https://tmo286.github.io/scripts-for-Qualtrics/fullaiateng.js
 
 	return iatExtension({
 		category2 : {
-			name : 'אמת', //Will appear in the data.
+			name : 'true', //Will appear in the data.
 			title : {
-				media : {word : 'אמת'}, //Name of the category presented in the task.
+				media : {word : 'true'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
